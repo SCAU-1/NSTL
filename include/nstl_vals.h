c1de80c0f089997671014e479a4b829377a43b29
czfshine <czfshine@outlook.com>
2017-08-11 09:14:56 +0800

    初始化代码

---
+++ include/nstl_vals.h
@@ -0,0 +1,12 @@
+/*********************************************
+* NSTL - Non-Standard Template Library.      *
+      Common Values
+* @Author: czfshine                           
+* @Date: 2017-08-09 08:35:32 
+*/
+
+#define NSTL_SPACENAME czfshine
+
+#define IN_NSTL namespace NSTL_SPACENAME {
+#define OUT_NSTL }
+#define NSTLSPACE ::NSTL_SPACENAME
---
+++ include/nstl_vector.h
@@ -0,0 +1,25 @@
+/*********************************************
+* NSTL - Non-Standard Template Library.      *
+      Containers -Vector core.
+* @Author: czfshine                           
+* @Date: 2017-08-10 09:52:38 
+*/
+
+#include "nstl_vals.h"
+
+IN_NSTL
+
+template <class T>
+class Vector{
+
+public:
+    typedef T value_type;
+    typedef T* iterator;
+private:
+
+
+public:
+    //
+
+}
+OUT_NSTL
